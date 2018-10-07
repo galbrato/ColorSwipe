7aaff979f7d0de9600709a494540196c2391beb4
Gabriel Toschi <gabriel.toschi@hotmail.com>
2018-10-07 02:19:49 -0300

    pause menu skeleton (works with Esc key)

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+	private bool isMenuOpen = false;
+	private GameObject menuObject;
+
+	void Start(){
+		menuObject = this.GetComponent<Transform>().Find("Menu").gameObject;
+	}
+
+	void changeState(){
+		isMenuOpen = !isMenuOpen;
+		menuObject.gameObject.SetActive(isMenuOpen);
+		if (isMenuOpen)
+			Time.timeScale = 0f;
+		else Time.timeScale = 1f;
+	}	
+
+	void Update () {
+		if (Input.GetKeyUp("escape")) changeState();
+	}
+}
